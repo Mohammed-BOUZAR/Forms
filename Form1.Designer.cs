2160f48bb9d93ee417005bc5afa2ad1fa9b7a7a7
Asus <Asus@DESKTOP-3GUL0VT>
2022-11-20 19:04:09 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnEnregistrer = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSelectionner = new System.Windows.Forms.Button();
+            this.textNom = new System.Windows.Forms.TextBox();
+            this.textPrenom = new System.Windows.Forms.TextBox();
+            this.textId = new System.Windows.Forms.TextBox();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.BackColor = System.Drawing.Color.Navy;
+            this.btnAjouter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAjouter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnAjouter.Location = new System.Drawing.Point(34, 386);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(82, 31);
+            this.btnAjouter.TabIndex = 0;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = false;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.BackColor = System.Drawing.Color.Navy;
+            this.btnModifier.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModifier.ForeColor = System.Drawing.Color.White;
+            this.btnModifier.Location = new System.Drawing.Point(133, 386);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(79, 31);
+            this.btnModifier.TabIndex = 1;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = false;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.BackColor = System.Drawing.Color.Red;
+            this.btnSupprimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSupprimer.ForeColor = System.Drawing.Color.White;
+            this.btnSupprimer.Location = new System.Drawing.Point(233, 386);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(83, 31);
+            this.btnSupprimer.TabIndex = 2;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = false;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // btnEnregistrer
+            // 
+            this.btnEnregistrer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnEnregistrer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnregistrer.ForeColor = System.Drawing.Color.White;
+            this.btnEnregistrer.Location = new System.Drawing.Point(583, 386);
+            this.btnEnregistrer.Name = "btnEnregistrer";
+            this.btnEnregistrer.Size = new System.Drawing.Size(84, 31);
+            this.btnEnregistrer.TabIndex = 3;
+            this.btnEnregistrer.Text = "Enregistrer";
+            this.btnEnregistrer.UseVisualStyleBackColor = false;
+            this.btnEnregistrer.Click += new System.EventHandler(this.btnEnregistrer_Click);
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.BackColor = System.Drawing.Color.Red;
+            this.btnAnnuler.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnnuler.ForeColor = System.Drawing.Color.White;
+            this.btnAnnuler.Location = new System.Drawing.Point(682, 386);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(85, 31);
+            this.btnAnnuler.TabIndex = 4;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = false;
+            this.btnAnnuler.Click += new System.EventHandler(this.btnAnnuler_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(479, 157);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // btnSelectionner
+            // 
+            this.btnSelectionner.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnSelectionner.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectionner.ForeColor = System.Drawing.Color.Teal;
+            this.btnSelectionner.Location = new System.Drawing.Point(604, 29);
+            this.btnSelectionner.Name = "btnSelectionner";
+            this.btnSelectionner.Size = new System.Drawing.Size(163, 54);
+            this.btnSelectionner.TabIndex = 6;
+            this.btnSelectionner.Text = "Selectionner";
+            this.btnSelectionner.UseVisualStyleBackColor = false;
+            this.btnSelectionner.Click += new System.EventHandler(this.btnSelectionner_Click);
+            // 
+            // textNom
+            // 
+            this.textNom.Location = new System.Drawing.Point(32, 257);
+            this.textNom.Name = "textNom";
+            this.textNom.Size = new System.Drawing.Size(128, 20);
+            this.textNom.TabIndex = 7;
+            // 
+            // textPrenom
+            // 
+            this.textPrenom.Location = new System.Drawing.Point(32, 296);
+            this.textPrenom.Name = "textPrenom";
+            this.textPrenom.Size = new System.Drawing.Size(128, 20);
+            this.textPrenom.TabIndex = 8;
+            // 
+            // textId
+            // 
+            this.textId.Location = new System.Drawing.Point(32, 219);
+            this.textId.Name = "textId";
+            this.textId.Size = new System.Drawing.Size(128, 20);
+            this.textId.TabIndex = 9;
+            this.textId.TextChanged += new System.EventHandler(this.textId_TextChanged);
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.BackColor = System.Drawing.Color.Red;
+            this.btnQuitter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuitter.ForeColor = System.Drawing.Color.White;
+            this.btnQuitter.Location = new System.Drawing.Point(604, 132);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(163, 54);
+            this.btnQuitter.TabIndex = 10;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = false;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.textId);
+            this.Controls.Add(this.textPrenom);
+            this.Controls.Add(this.textNom);
+            this.Controls.Add(this.btnSelectionner);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.btnEnregistrer);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnAjouter);
+            this.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.Name = "Form1";
+            this.Text = "Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnEnregistrer;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSelectionner;
+        private System.Windows.Forms.TextBox textNom;
+        private System.Windows.Forms.TextBox textPrenom;
+        private System.Windows.Forms.TextBox textId;
+        private System.Windows.Forms.Button btnQuitter;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms
+{
+    public partial class Form1 : Form
+    {
+
+        static string chaine = @"Data Source=DESKTOP-3GUL0VT;Initial Catalog=DOSSIER;;User ID=sa;Password=Mohammed123";
+        static SqlConnection cnx = new SqlConnection(chaine);
+        static SqlCommand cmd = new SqlCommand();
+        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        public Form1()
+        {
+            InitializeComponent();
+            btnSupprimer.Enabled = false;
+            btnModifier.Enabled = false;
+            btnEnregistrer.Enabled = false;
+            btnAnnuler.Enabled = false;
+            textId.Enabled = false;
+            textNom.Enabled = false;
+            textPrenom.Enabled = false;
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            btnEnregistrer.Enabled = true;
+            btnAnnuler.Enabled = true;
+            btnSelectionner.Enabled = false;
+            btnModifier.Enabled = false;
+            btnSupprimer.Enabled = false;
+            textNom.Enabled = true;
+            textNom.Focus();
+            textPrenom.Enabled = true;
+            dataGridView1.DataSource = null;
+        }
+
+        private void btnSelectionner_Click(object sender, EventArgs e)
+        {
+            btnSupprimer.Enabled = true;
+            btnModifier.Enabled = true;
+            cnx.Open();
+            cmd.CommandText = "select * from dataform";
+            cmd.Connection = cnx;
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            cnx.Close();
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            btnAjouter.Enabled = false;
+            btnSupprimer.Enabled = false;
+            btnEnregistrer.Enabled = true;
+            btnSelectionner.Enabled = false;
+            btnAnnuler.Enabled = true;
+            textId.Enabled = true;
+            textId.Focus();
+            textNom.Enabled = true;
+            textPrenom.Enabled = true;
+        }
+
+        private void textId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnregistrer_Click(object sender, EventArgs e)
+        {
+            cnx.Open();
+            cmd.Connection = cnx;
+            if (btnAjouter.Enabled) {
+                cmd.CommandText = "insert into dataform (nom, prenom) values('" + textNom.Text + "','" + textPrenom.Text + "') ";
+                textNom.Text = "";
+                textPrenom.Text = "";
+                textNom.Enabled = false;
+                textPrenom.Enabled = false; 
+                btnAnnuler.Enabled = false;
+            }
+            if (btnModifier.Enabled) {
+                if(textId.Text != "")
+                    cmd.CommandText = "update dataform set nom='" + textNom.Text + "' ,prenom='" + textPrenom.Text + "' where id = " + textId.Text + ";";
+                btnAjouter.Enabled = true;
+                textId.Text = "";
+                textNom.Text = "";
+                textPrenom.Text = "";
+                textId.Enabled = false; 
+                textNom.Enabled = false;
+                textPrenom.Enabled = false; 
+            }
+            if (btnSupprimer.Enabled)
+            {
+                if (textId.Text != "")
+                    cmd.CommandText = "delete from dataform where id = " + textId.Text + ";";
+                textId.Text = "";
+                textId.Enabled = false;
+                btnAjouter.Enabled = true;
+            }
+            cmd.ExecuteNonQuery();
+            cnx.Close();
+            btnEnregistrer.Enabled = false;
+            btnAnnuler.Enabled = false;
+            btnSelectionner.Enabled = true;
+            btnSupprimer.Enabled = false;
+            btnModifier.Enabled = false;
+            dataGridView1.DataSource = null;
+        }
+
+        private void btnAnnuler_Click(object sender, EventArgs e)
+        {
+            if (btnAjouter.Enabled)
+            {
+                textNom.Text = "";
+                textPrenom.Text = "";
+                textNom.Enabled = false;
+                textPrenom.Enabled = false;
+            }
+            if (btnModifier.Enabled)
+            {
+                textId.Text = "";
+                textNom.Text = "";
+                textPrenom.Text = "";
+                textId.Enabled = false;
+                textNom.Enabled = false;
+                textPrenom.Enabled = false;
+                btnAjouter.Enabled = true;
+            }
+            if (btnSupprimer.Enabled)
+            {
+                textId.Text = "";
+                textId.Enabled = false;
+                btnAjouter.Enabled = true;
+            }
+            if(dataGridView1.DataSource != null)
+            {
+                btnSupprimer.Enabled = false;
+                btnModifier.Enabled = false;
+                dataGridView1.DataSource = null;
+            }
+            btnEnregistrer.Enabled = false;
+            btnAnnuler.Enabled = false;
+            btnSelectionner.Enabled = true;
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            textId.Enabled = true;
+            btnEnregistrer.Enabled = true;
+            btnAnnuler.Enabled = true;
+            btnAjouter.Enabled = false;
+            btnModifier.Enabled = false;
+            btnSelectionner.Enabled = false;
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
